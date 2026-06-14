4bda39aa034810ae361240aba0c66f1b87643fc8
klarissaslbn <klarissasilabann@gmail.com>
2026-06-14 22:48:13 +0700

    Add files via upload

---
+++ pertemuan10-javascript/script.js
@@ -0,0 +1,10 @@
+/*
+EXTERNAL JavaScript
+Menggunakan console.log dan alert
+*/
+
+// console.log
+console.log("Output dari EXTERNAL JavaScript");
+
+// alert
+alert("Ini alert dari EXTERNAL JavaScript");
